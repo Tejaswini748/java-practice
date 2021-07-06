7b4e8ac3f212d2ed34e5fa0f824655b47b65405c
Tejaswini748 <tejuthakare.tt@gmail.com>
2021-07-06 23:21:30 +0530

    added

--- programs/Leapyr.java
+++ programs/Leapyr.java
@@ -5,9 +5,13 @@ public class Leapyr {
      Scanner sc=new Scanner(System.in);
      System.out.println("Enterthe year");
      year=sc.nextInt();
-     if(year%4==0){
+     if((year%4==0) &&(year%100!=0)){
+         if(year%100!=0){
+           
          System.out.println("Year is leap year");
-     }else
+                
+        }
+    }else
      {
          System.out.println ("Year is not a leap year");
      }
--- programs/VC.java
+++ programs/VC.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 public class VC {
   public static void main(String[] args) {
-      char ch;
+    //   char ch;
 Scanner sc= new Scanner(System.in);
 System.out.println("Enter your character");
-String str =sc.next();
-ch= str.charAt(0);
-if(ch=='a'|| ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'|| ch=='E'||ch=='I'||ch=='O'||ch=='U'){
+String ch =sc.next();
+// ch= str.charAt(0);//
+if(ch=="a"|| ch=="e"||ch=="i"||ch=="o"||ch=="u"||ch=="A"|| ch=="E"||ch=="I"||ch=="O"||ch=="U"){
     System.out.println("Entered character is vowel");
 }else{
 
